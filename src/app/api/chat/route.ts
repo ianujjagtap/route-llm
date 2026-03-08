9b4ab9c3f23779b81b332154a350ced00a18f55b
ianujjagtap <anujjagtap2004@gmail.com>
2026-03-08 17:15:21 +0530

    fix(api): clean up unused line

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,7 +4,6 @@ import { convertToCoreMessages, smoothStream, streamText } from "ai";
 // streaming responses upto 30 seeconds
 export const maxDuration = 30;
 
-
 export async function POST(req: Request) {
   const { messages } = await req.json();
   const result = streamText({
