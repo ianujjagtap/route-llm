59ecda953c918651ae4e9cb89ace8203321f233a
ianujjagtap <anujjagtap2004@gmail.com>
2026-03-08 17:13:18 +0530

    fix(api): use convertToCoreMessages for AI sdk payload parsing

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,19 +1,15 @@
 import { google } from "@ai-sdk/google";
-import { smoothStream, streamText } from "ai";
+import { convertToCoreMessages, smoothStream, streamText } from "ai";
 
 // streaming responses upto 30 seeconds
 export const maxDuration = 30;
 
-interface Message {
-  role: "user" | "assistant";
-  content: string;
-}
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
   const result = streamText({
     model: google("gemini-2.5-flash"),
-    prompt: messages.map((m: Message) => m.content).join("\n"),
+    messages: convertToCoreMessages(messages),
     experimental_transform: smoothStream({ delayInMs: 100 }),
   });
   return result.toUIMessageStreamResponse();
